d4094eea723dd1684084c7f65f2522ed5fac20e3
tangxiaoxin <tangxiaoxin@kuaishou.com>
2025-02-27 19:12:04 +0800

    feat: 切换redirect uri

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -1,4 +1,4 @@
-import { transformFigmaToHtml, transformFigmaToHtmlBatch } from "figma-to-html";
+import { transformFigmaToHtml } from "figma-to-html";
 import "./App.css";
 import { useCallback, useState } from "react";
 
@@ -6,21 +6,7 @@ function App() {
   const [address, setAddress] = useState<string>("");
   const [result, setResult] = useState<string>("generate...");
   const generate = useCallback(async () => {
-    console.time("开始转换");
-    transformFigmaToHtmlBatch(
-      [
-        "https://www.figma.com/design/RIXZ57M73YaV4qONoDrtbh/UI-潮流服饰频道?node-id=441-2955&t=FEgDqVhDLwsoBiRN-4",
-        "https://www.figma.com/design/RIXZ57M73YaV4qONoDrtbh/UI-潮流服饰频道?node-id=78-4056&t=TFfnF9S6CrCX6Z4y-4",
-        "https://www.figma.com/design/RIXZ57M73YaV4qONoDrtbh/UI-潮流服饰频道?node-id=5-452&t=TFfnF9S6CrCX6Z4y-4",
-        "https://www.figma.com/design/RIXZ57M73YaV4qONoDrtbh/UI-潮流服饰频道?node-id=34-3494&t=TFfnF9S6CrCX6Z4y-4",
-      ],
-      (process: number) => {
-        console.log(`进度: ${(process * 100).toFixed(2)}%`);
-      }
-    ).then((res) => {
-      console.log(res);
-      console.timeEnd("开始转换");
-    });
+    console.time("transformFigmaToHtml");
     if (!address) {
       return;
     }
@@ -28,6 +14,7 @@ function App() {
       console.log(`进度: ${(process * 100).toFixed(2)}`);
     }).then((res) => {
       setResult(res);
+      console.timeEnd("transformFigmaToHtml");
     });
   }, [address]);
 
